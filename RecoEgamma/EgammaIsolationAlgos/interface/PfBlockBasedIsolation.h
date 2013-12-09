5f7861ce253f7b38df7224b89230c75c214fa0bb
Nancy <nancy.marinelli@cern.ch>
2013-12-09 13:38:15 +0100

    First commit: algorithm for calculating particle based isolation for photons and electrons

---
+++ RecoEgamma/EgammaIsolationAlgos/interface/PfBlockBasedIsolation.h
@@ -0,0 +1,66 @@
+//--------------------------------------------------------------------------------------------------
+// $Id $
+//
+//  PfBlockBasedIsolationCalculator.cc
+// Authors: N. Marinelli Univ. of Notre Dame
+//--------------------------------------------------------------------------------------------------
+
+
+#ifndef PfBlockBasedIsolation_H
+#define PfBlockBasedIsolation_H
+
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PileUpPFCandidate.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PileUpPFCandidateFwd.h"
+
+#include "DataFormats/EgammaCandidates/interface/Photon.h"
+#include "DataFormats/EgammaCandidates/interface/PhotonFwd.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
+
+
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
+
+
+class PfBlockBasedIsolation{
+ public:
+  //  PfBlockBasedIsolation(const reco::Photon);
+  // PfBlockBasedIsolation(const reco::GsfElectron);
+  PfBlockBasedIsolation();
+
+
+  ~PfBlockBasedIsolation();
+
+
+
+  void setup(const edm::ParameterSet& conf);
+  
+
+
+ public:
+
+  
+
+   std::vector<std::pair<reco::PFCandidateRef, bool>> calculate(math::XYZTLorentzVectorD p4,
+		 const reco::PFCandidateRef pfEGCand,
+		 const edm::Handle<reco::PFCandidateCollection> pfCandidateHandle);
+
+  
+ 
+ private:
+
+     
+
+};
+
+#endif
